1f27c62e0c40979f77d7ed3fccfda6a50a8b75b7
wagner.dutra <wagnerdutra1010@gmail.com>
2019-07-23 13:08:38 -0300

    Adding Sentry and Metrics controloller

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const validator = require('express-validation')
 const handler = require('express-async-handler')
 
 const ScoreController = require('./app/controllers/ScoreController')
+const MetricsController = require('./app/controllers/MetricsController')
 
 const ScoreValidation = require('../src/app/validators/Score')
 
@@ -17,5 +18,9 @@ routes.post(
   validator(ScoreValidation.update),
   handler(ScoreController.update)
 )
+
+routes.get('/health', handler(MetricsController.health))
+
+routes.get('/ready', MetricsController.ready)
 
 module.exports = routes
